98535f598436ad86878ad70eb796e39c5c853f93
lixinxin <895330766@qq.com>
2018-09-07 11:25:57 +0800

    添加 anko 库

--- UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
@@ -3,11 +3,23 @@ package com.kotlin.user.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.kotlin.user.R
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.intentFor
+
+import org.jetbrains.anko.toast
+
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        mBtnRegister.setOnClickListener {
+            toast("ll")
+            startActivity(intentFor<TestActivity>("id" to "oo"))
+        }
+
+
     }
 }
