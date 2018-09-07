2d5ea2dcf06a065cf35ee156c76979fa75b93ddc
lixinxin <895330766@qq.com>
2018-09-07 17:53:40 +0800

    添加 rx 依赖

--- BaseLibrary/src/main/java/com/kotlin/base/ui/activity/BaseMvpActivity.kt
+++ BaseLibrary/src/main/java/com/kotlin/base/ui/activity/BaseMvpActivity.kt
@@ -4,6 +4,7 @@ import com.kotlin.base.presenter.BasePresenter
 import com.kotlin.base.presenter.view.BaseView
 
 open class BaseMvpActivity<T:BasePresenter<*>> :BaseActivity(),BaseView {
+
     override fun showLoading() {
     }
 
